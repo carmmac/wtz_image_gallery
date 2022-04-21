9b31b9dfc6d0f7aecb9a3947428e2c9257634e1e
bremxx <petr.kirienko@gmail.com>
2022-04-21 22:07:08 +0300

    views

---
+++ src/views/MainView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="gallery-wrapper">
+    <image-gallery v-if="isDataLoaded" />
+    <loading v-else />
+  </div>
+</template>
+
+<script>
+import { ActionType } from '@/store/actions';
+import { State } from '@/store/state';
+import { mapActions, mapState } from 'vuex';
+import Loading from '../components/loading.vue';
+import ImageGallery from '../components/image-gallery.vue';
+
+export default {
+  name: 'MainView',
+  components: { ImageGallery, Loading },
+  created() {
+    this.loadPhotos();
+  },
+  computed: {
+    ...mapState({
+      isDataLoaded: State.IS_DATA_LOADED,
+    }),
+  },
+  methods: {
+    ...mapActions({
+      loadPhotos: ActionType.FETCH_PHOTOS,
+    }),
+  },
+};
+</script>
