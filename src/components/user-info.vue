e4636d49665efab0bbf6f4e4bcbae77436e59b20
bremxx <petr.kirienko@gmail.com>
2022-04-22 21:44:34 +0300

    User view refactored

--- src/components/user-info.vue
+++ src/components/user-info.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="user-info">
-    <img class="user-img" :src="getUserAvatar" alt="Avatar" width="30" height="30" />
+  <div :class="`user-info ${getStyles}`">
+    <img class="user-img" :src="getAvatarUrl(windowWidth)" alt="User avatar" />
     <div>
-      <div class="user-title">{{ getUserFullName }}</div>
+      <h2 class="user-name">{{ getUserFullName }}</h2>
       <div class="user-instagram">{{ getUserSocialLink }}</div>
     </div>
   </div>
@@ -10,9 +10,16 @@
 
 <script>
 import { getSocialUsername } from '@/_utils';
+import { AppRoutes, BreakPoint } from '@/const';
 
 export default {
   name: 'UserInfo',
+  data() {
+    return {
+      userPageRoute: this.$route.name === AppRoutes.USER_PAGE.name,
+      windowWidth: window.innerWidth,
+    };
+  },
   props: {
     user: {
       type: Object,
@@ -26,7 +33,17 @@ export default {
     getUserSocialLink() {
       return this.user.instagram_username && getSocialUsername(this.user.instagram_username);
     },
-    getUserAvatar() {
+    getStyles() {
+      return this.userPageRoute ? 'user-info--user-page' : '';
+    },
+  },
+  methods: {
+    getAvatarUrl(screenWidth) {
+      if (this.userPageRoute) {
+        return screenWidth >= BreakPoint.TABLET
+          ? this.user.profile_image.large
+          : this.user.profile_image.medium;
+      }
       return this.user.profile_image.small;
     },
   },
--- src/components/user-profile.vue
+++ src/components/user-profile.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
-    <header class="user-header">
-      <div class="user-info">
-        <h1 class="user-name">{{ getUserFullName }}</h1>
-        <span class="user-instagram">{{ getUserSocialLink }}</span>
-      </div>
-      <img
-        class="user-img user-img--user-page"
-        :src="getAvatarUrl(windowWidth)"
-        alt="User profile photo"
-      />
-    </header>
+    <user-info :user="user" />
     <div class="user-details">
       <span v-if="user.bio" class="user-details__item">{{ user.bio }}</span>
       <span v-if="user.location" class="user-details__item">Location: {{ user.location }}</span>
@@ -22,35 +12,17 @@
 </template>
 
 <script>
-import { BreakPoint } from '@/const';
 import { State } from '@/store/state';
 import { mapState } from 'vuex';
-import { getSocialUsername } from '@/_utils';
+import UserInfo from './user-info.vue';
 
 export default {
   name: 'UserProfile',
-  data() {
-    return {
-      windowWidth: window.innerWidth,
-    };
-  },
+  components: { UserInfo },
   computed: {
     ...mapState({
       user: State.USER_PROFILE,
     }),
-    getUserFullName() {
-      return `${this.user.first_name} ${this.user.last_name ?? ''}`;
-    },
-    getUserSocialLink() {
-      return this.user.instagram_username && getSocialUsername(this.user.instagram_username);
-    },
-  },
-  methods: {
-    getAvatarUrl(screenWidth) {
-      return screenWidth >= BreakPoint.TABLET
-        ? this.user.profile_image.large
-        : this.user.profile_image.medium;
-    },
   },
 };
 </script>
